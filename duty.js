f9b9bfff4eeb8671b591edf6ca03d7e3dcccd75d
NovatePals <novatepals@gmail.com>
2023-10-18 22:30:03 +0800

    error of date fixed

--- duty.js
+++ duty.js
@@ -1,26 +1,31 @@
 const dutyRoster = [
   {
     day: "Monday",
+    dayIndex: 1,
     location: "MBSJ FIELD",
     duty: "Collecting Rubbish",
   },
   {
     day: "Tuesday",
+    dayIndex: 2,
     location: "CYBERIA APARTMENT",
     duty: "Cleaning Rubbish Bins",
   },
   {
     day: "Wednesday",
+    dayIndex: 3,
     location: "MMU UNIVERSITY",
     duty: "Change Bins",
   },
   {
     day: "Thursday",
+    dayIndex: 4,
     location: "DPULZE",
     duty: "Collecting Rubbish",
   },
   {
     day: "Friday",
+    dayIndex: 5,
     location: "KEDAI MAMAK",
     duty: "Eat Rubbish",
   },
@@ -31,16 +36,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const rosterBody = document.getElementById("roster-body");
   const today = new Date();
 
-  // Function to format the date based on the day index (0 for Sunday, 1 for Monday, etc.)
+  // Function to format the date based on the day index
   function formatDate(date, dayIndex) {
-    const daysToAdd = dayIndex - date.getDay();
+    const currentDay = date.getDay();
+    const daysToAdd = dayIndex - currentDay;
     date.setDate(date.getDate() + daysToAdd);
     return date.toLocaleDateString();
   }
 
   // Loop through the duty roster data and create rows in the table
-  dutyRoster.forEach((entry, index) => {
-    const date = formatDate(today, index); // Calculate the date
+  dutyRoster.forEach((entry) => {
+    const date = formatDate(today, entry.dayIndex); // Calculate the date
     const row = document.createElement("tr");
     row.innerHTML = `
           <td>${entry.day}</td>
